18d43423e8d55614081dfedbb701e16831b5905f
Zach Giffen <zach.giffen@yahoo.com>
2023-10-23 18:30:35 +0000

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+const fullName = 'Zach Giffen';
+const isCool = true;
+const totalPets = 4;
+
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
+
+const unknown = null;
+
+console.log(unknown);
+console.log(typeof unknown);
+
+let never;
+
+console.log(never);
+console.log(typeof never);
