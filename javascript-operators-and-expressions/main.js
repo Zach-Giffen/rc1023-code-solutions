fb01b164f36dfd58ae9a45a1cc5b63801babcf51
Zach Giffen <zach.giffen@yahoo.com>
2023-10-23 20:53:07 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,53 @@
+const width = 3;
+const height = 5;
+
+const area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 2;
+const payment = 7;
+
+const change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change', typeof change);
+
+const quizzes = 50;
+const midterm = 20;
+const final = 100;
+
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Zach';
+const lastName = 'Giffen';
+
+const fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 9;
+
+const isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 100;
+
+const isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+
+motto += ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
