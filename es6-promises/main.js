da28054d516eec0e61a3c763ac2293b365be51cc
Zach Giffen <zach.giffen@yahoo.com>
2023-12-12 19:50:43 +0000

    added message

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -1,3 +1,5 @@
 import { takeAChance } from './take-a-chance.js';
 const promise = takeAChance('Zach');
-promise.then((res) => console.log(res)).catch((err) => console.log(err));
+promise
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err.message));
